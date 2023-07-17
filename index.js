18bc9f2a0ca65db72a1f6f0999c45064366e51f6
Elienay Alves <elienay8486@gmail.com>
2023-07-17 11:56:07 -0300

    feat(index.js): inicialize the account creation system
    We created an arrow function that is called on line 19 when the action of the user is equal to
    'Criar Conta', when this happens we thank the user for choose our bank ask him to define his account
    options

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// modulos externos
+// external modules
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 
-// modulos internos
+// internal modules
 const fs = require('fs');
 
 const operation = () => {
@@ -11,20 +11,20 @@ const operation = () => {
       type: 'list',
       name: 'action',
       message: 'O que você deseja fazer:',
-      choices: [
-        'Criar Conta',
-        'Consultar Saldo',
-        'Depositar',
-        'Sacar',
-        'Sair',
-      ],
+      choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
     },
   ]).then((answer) => {
     const { action } = answer;
-
-    console.log(action);
-  })
-  .catch((err) => console.log(err));
+    if (action === 'Criar Conta') {
+      createAccount();
+    }
+  }).catch((err) => console.log(err));
 };
 
 operation();
+
+// create an account
+const createAccount = () => {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
+};
