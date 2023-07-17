dec747e275f9237047a5deea6e88f89c6fe49f80
Elienay Alves <elienay8486@gmail.com>
2023-07-17 14:44:12 -0300

    feat(index.js): finalize account creattion

--- src/account/createAccount.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-// create an account
-const createAccount = () => {
-  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
-  console.log(chalk.green('Defina as opções da sua conta a seguir'));
-};
-
-module.exports = createAccount;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // external modules
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 
 // internal modules
 const fs = require('fs');
-const createAccount = require('./account/createAccount');
 
 const operation = () => {
   inquirer.prompt([
@@ -22,3 +22,39 @@ const operation = () => {
 };
 
 operation();
+
+const createAccount = () => {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+  console.log(chalk.green('Defina as opções da sua conta a seguir'));
+  buildAccount();
+};
+
+const buildAccount = () => {
+  inquirer.prompt([
+    {
+      name: 'accountName',
+      message: 'Digite um nome para a sua conta:',
+    },
+  ]).then((answer) => {
+    const { accountName } = answer;
+
+    console.info(accountName);
+
+    if (!fs.existsSync('src/accounts')) {
+      fs.mkdirSync('src/accounts');
+    }
+
+    if (fs.existsSync(`src/accounts/${accountName}.json`)) {
+      console.log(chalk.bgRed.black('Essa conta já existe, escolha outro nome!'));
+      buildAccount();
+      return;
+    }
+
+    fs.writeFileSync(`src/accounts/${accountName}.json`, '{"balance": 0}', (err) => console.log(err));
+
+    console.log(chalk.green('Parabéns, a sua conta foi criada!'));
+    operation();
+  }).catch((err) => console.log(err));
+};
+
+module.exports = buildAccount;
